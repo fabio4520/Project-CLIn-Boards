b00c6513364125f95b46bc7627425e8f8af5f23d
Jesus Quijano <jesusqjn@gmail.com>
2022-03-03 23:29:43 -0500

    update board

--- board.rb
+++ board.rb
@@ -8,5 +8,30 @@ class Board
     @list = list
   end
 
+  #{name: @name, description: @description}
+  def to_json
+    {id:=@id, name: @name, description: @description, list: @list}
+  end
+
+  def update_board(data)
+    data[:name].empty? == true ? data[:name]=@name : data[:name]=name
+    data[:description].empty? == true ? data[:description]=@description : data[:description]=description
+  end
+
+  def verificación_id
+    id = ""
+     while  id.empty?
+       print "ID: "
+       id = gets.chomp.to_i
+         if id!=@id
+           print "ID Incorrect\n"
+           id=""
+         else
+          break
+         end 
+     end 
+     id 
+  end
+
 end
 
